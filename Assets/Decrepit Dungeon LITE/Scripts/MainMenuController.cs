81b6028d5831ed210fa6fcd51b5df628c538cfa5
lanaarafat <a.mutoni1@alustudent.com>
2024-10-18 19:25:24 +0200

    dungeon

---
+++ Assets/Decrepit Dungeon LITE/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void PlayGame() {
+		SceneManager.LoadScene ("AssignmentOneScene");
+	}
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+
+    }
+}
